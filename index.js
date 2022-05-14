0eff547689894ceeec6d46682cd95058ebda0fed
liceal <liceal@163.com>
2022-05-14 11:34:22 +0800

    2 files modified

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-// run `node index.js` in the terminal
+/**
+ * @param {String} word 说话内容
+ */
+function say(word) {
+  console.log(word);
+}
 
-console.log(`Hello Node.js v${process.versions.node}!11`);
+function* fn1() {
+  yield say('hi');
+  yield say('my name is yield');
+  yield say('count down for 5 start!');
+  for (let i = 1; i >= 5; i++) {
+    yield say(i);
+  }
+  yield say('ok! say over');
+}
+
+const Sandy = fn1();
+
+Sandy.next();
+Sandy.next();
